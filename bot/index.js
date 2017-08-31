6fc2f6ec7ef820997f15829eb82dfe34b47965bc
shadab <shadab@shadabs-MacBook-Pro.local>
2017-09-01 03:56:27 +0430

    at least fucks once

--- bot/index.js
+++ bot/index.js
@@ -6,6 +6,7 @@ var bot=botgram('431527109:AAECtqEPZnrglz1VKYJubAuLBzdKcIQmqq8');
 var fucks=['him','her','this','you','me','it','everything','class','the school','UMZ','Oracle','gradle','xCode','expo','callback','emulator','the hindi guy who creates tutorial','google speech api','developer.android','sqlite']
 
 bot.command('start',function(msg,reply,next){
+    sendFuck()
     function sendFuck(){
         var index= Math.floor((Math.random() * (fucks.length-1)) + 0);
         var thisFuck=fucks[index];
@@ -13,5 +14,5 @@ bot.command('start',function(msg,reply,next){
     }
     // setInterval(sendFuck, 3000);
     
-    setInterval(sendFuck, Math.floor((Math.random() * 18000000) + 5000));
+    setInterval(sendFuck, Math.floor((Math.random() * 2000000) + 5000));
 })
